3acf9357ac11f9fffc20a79592d5423740f7e2be
xiyan <37499101+xiyanma@users.noreply.github.com>
2023-03-13 16:08:50 +0800

    Update data-structure.js

--- data-structure.js
+++ data-structure.js
@@ -1,20 +1,20 @@
 /* 栈实现队列 */
-class queue {
+class Queue {
     constructor() {
-        this.stack1 = []
-        this.stack2 = []
+        this.stack1 = [];
+        this.stack2 = [];
     }
     in(val) {
-        this.stack1.push(val)
+        this.stack1.push(val);
     }
     out() {
         while (this.stack1.length) {
-            this.stack2.push(this.stack1.pop())
-        }
-        return this.stack2.pop()
+            this.stack2.push(this.stack1.pop());
+        };
+        return this.stack2.pop();
     }
 }
-const myQueue = new queue();
+const myQueue = new Queue();
 myQueue.in(1);
 myQueue.in(2);
 myQueue.in(3);
